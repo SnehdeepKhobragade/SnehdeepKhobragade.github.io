a535675d18c10a7601bcc60c3fcf77d1a6928ac4
SnehdeepKhobragade <snehdeepkhobragade95@gmail.com>
2023-02-15 14:36:36 +0530

    hii

--- index.js
+++ index.js
@@ -15,9 +15,5 @@ tabs.forEach(tab => {
     event.currentTargetclassList.add("active-link");
 
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
-
-  });
-});
-
+    src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'>
 </script>
